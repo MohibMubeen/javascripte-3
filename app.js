af43d52448aa9edce91b48e84626665680c2f2b1
ComTech <mohibmubeen058@gmail.com>
2023-02-04 17:28:46 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var we = "<h3> QUESTION#1 </h3>"
+document.write(we)
+var tr = "sum 3 and 5"
+var rt = "is 8"
+document.write(tr+rt)
+var hg = "<h3> QUESTION#2 </h3>"
+document.write(hg)
+var yu = "subtract 3 and 5"
+var ew = "is -5"
+document.write(yu+ew)
+var fr = " <br> multiply 3 and 5"
+var qw = "is 15"
+document.write(fr+qw)
+var hi = "<br> divide 3 and 5"
+var lk = "is 0.6"
+document.write(hi+lk)
+var uf = ""
